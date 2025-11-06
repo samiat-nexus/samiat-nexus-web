b6614dddf0afe2b8e22932731fb8b4a62295937d
samiat-nexus <contact.samiatnexus@gmail.com>
2025-11-06 21:31:46 +0600

    script.js

--- js/script.js
+++ js/script.js
@@ -95,3 +95,12 @@ if (form) {
     }, 3000);
   });
 }
+
+
+// JS toggle logic
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav-links");
+
+  toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+  });
